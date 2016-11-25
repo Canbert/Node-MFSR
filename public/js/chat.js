5b293c905076b3758256e8f0492c3020e3e9c631
canbert <thompus987@gmail.com>
2016-11-25 04:02:59 +0000

    Added timestamp to message

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,17 @@
 var socket = io();
 $('form').submit(function(){
-    socket.emit('userMsg', {username: "test"/*user.username*/, message: $('#msg').val()});
+
+    var date = new Date();
+    var timestamp = date.toString();
+
+    socket.emit('userMsg', {username: "test"/*user.username*/, message: $('#msg').val(), timestamp: timestamp});
     $('#msg').val('');
     return false;
 });
 socket.on('userMsg', function(msg){
     $('#messages').append($(
         '<div class="callout">' +
+        '<sup>' + msg.timestamp + '</sup></br>' +
         '<b>' + msg.username + ': </b>' +
         msg.message +
         '</div>'));

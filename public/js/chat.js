6849fadc0fa3bc504f80f022abdddb0b387f76e0
canbert <thompus987@gmail.com>
2016-11-26 04:00:17 +0000

    Added notifications closes #4

--- public/js/chat.js
+++ public/js/chat.js
@@ -34,6 +34,9 @@ $('#msg').on('keydown', function(event){
 });
 
 socket.on('userMsg', function(msg){
+    if(msg.username != user.username){ //only send notification if not from self
+        notifyMe(msg);
+    }
     $('#messages').append($(
         '<div class="callout">' +
         '<sup>' + msg.timestamp + '</sup></br>' +
@@ -63,4 +66,36 @@ function htmlDecode(input){
     var e = document.createElement('div');
     e.innerHTML = input;
     return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+}
+
+function notifyMe(msg) {
+
+    var title = "MFSR";
+    var options = {
+        body: msg.message
+    }
+
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        var notification = new Notification(title,options);
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function (permission) {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+                var notification = new Notification(title,options);
+            }
+        });
+    }
+
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them any more.
 }

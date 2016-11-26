bda6a71afc66b6d7943feee6e0420f09e562e766
canbert <thompus987@gmail.com>
2016-11-26 02:10:59 +0000

    Added users online

--- app/chat.js
+++ app/chat.js
@@ -1,9 +1,22 @@
 
 module.exports = function (io) {
 
+    var users = [];
+
     io.on('connection', function (socket) {
         console.log('Socket.io chat: user connected');
         io.emit('serverMsg', 'User connected');
+
+        socket.on('userConnect',function (data, fn) {
+            if (users.indexOf(data) != -1){
+                fn(false);
+            } else {
+                fn(true);
+                socket.user = data;
+                users.push(socket.user);
+                io.sockets.emit('userConnect', users);
+            }
+        });
 
         socket.on('userMsg', function (msg) {
             console.log("Socket.io chat: " + msg.username + ' said ' + msg.message);
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,17 @@
 var socket = io();
+
+var $users = $("#active-users");
+
+$(document).ready(function () {
+   socket.emit('userConnect', user.username, function(data){
+       if(data){
+           console.log("yep");
+       }else{
+           console.log("nope");
+       }
+   });
+});
+
 $('form').submit(function(){
 
     var date = new Date();
@@ -24,6 +37,14 @@ socket.on('serverMsg', function(msg){
         '<button class="close-button" aria-label="Dismiss alert" type="button" data-close>' +
         '<span aria-hidden="true">&times;</span>' +
         '</button></div>'));
+});
+
+socket.on('userConnect', function (data){
+    var html = '';
+    for (i=0; i < data.length; i++){
+        html += data[i] + '<br/>';
+        $users.html(html);
+    }
 });
 
 function htmlDecode(input){
